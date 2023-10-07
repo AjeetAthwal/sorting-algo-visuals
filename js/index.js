49e79571311b50e7c7d341edfd813ba862542788
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2023-10-08 00:06:23 +0100

    initialise basic file structure

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch data from data.json
+    fetch('data/data.json')
+        .then(response => response.json())
+        .then(data => {
+            // Display the message in the HTML
+            document.getElementById('hello-message').innerText = data.message;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
